38a14c514760aa614eb1477e12fe2366c6d322c8
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-20 15:35:13 +0800

    feat: stylelint 配置

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
+import { Fragment } from 'vue'
 </script>
 
 <template>
-  <header>
-    <h1>Application</h1>
-  </header>
-  <main>
-    <h2>Content</h2>
-  </main>
+  <Fragment class="container">
+    <header>
+      <h1>Application</h1>
+    </header>
+    <main>
+      <h2>Content</h2>
+    </main>
+  </Fragment>
 </template>
 
 <style scoped>
+.container {
+  width: 600px;
+  height: 400px;
+  background: skyblue;
+  h1 {
+    color: red;
+  }
+}
 </style>

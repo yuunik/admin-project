8301c0e5a58dbd2ac66484ebeae27a551e5f7c53
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-20 14:36:54 +0800

    feat: init Project.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1>Application</h1>
+  </header>
+  <main>
+    <h2>Content</h2>
+  </main>
+</template>
+
+<style scoped>
+</style>

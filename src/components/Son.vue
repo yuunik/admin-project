87be92484510d8ebdb8ff0c42ae0a79886088c3c
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-20 17:13:15 +0800

    feat: svg 图标配置 todo

--- src/components/Son.vue
+++ src/components/Son.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log(import.meta.env)
+</script>
 
 <template>
   <h1>我是 son</h1>
+  <svg>
+    <use xlink-href="#icon-work"></use>
+  </svg>
 </template>
 
 <script scoped lang="scss"></script>
---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  prefix: {
+    type: string
+    default: '#icon-'
+  }
+  name: string
+  color: {
+    type: string
+    default: ''
+  }
+  width: {
+    type: string
+    default: '16px'
+  }
+  height: {
+    type: string
+    default: '16px'
+  }
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <Fragment>
+    <svg :style="{ width: width, height: height }">
+      <use :xlink:href="prefix + name" :fill="color"></use>
+    </svg>
+  </Fragment>
+</template>
+
+<style scoped lang="scss"></style>

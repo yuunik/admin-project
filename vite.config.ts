6c599f7a5e659f1217c7a454fe70cc04ad224f68
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-21 11:24:46 +0800

    feat:  mock 数据配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,37 +6,44 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
 // svg 图标插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+// mockjs 插件
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // element-plus 自动导入与按需导入
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // Specify symbolId format
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
-    },
-  },
-  // 样式配置
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "@/styles/variable.scss";',
+export default defineConfig(() => {
+  return {
+    plugins: [
+      vue(),
+      // element-plus 自动导入与按需导入
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      // svg 插件
+      createSvgIconsPlugin({
+        // Specify the icon folder to be cached
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // Specify symbolId format
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      // mockjs 插件
+      viteMockServe(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
       },
     },
-  },
+    // 样式配置
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "@/styles/variable.scss";',
+        },
+      },
+    },
+  }
 })

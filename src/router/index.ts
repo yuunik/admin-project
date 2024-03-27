5cc1dca49bb7619b43990baf89d475fb75922433
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-27 15:57:34 +0800

    feat: layout - 进度条业务实现

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // 引入路由表
 import routes from './routes'
+// 引入进度条插件
+import nprogress from 'nprogress'
+// 引入进度条样式
+import 'nprogress/nprogress.css'
 
 // 路由器
 const router = createRouter({
@@ -15,8 +19,21 @@ const router = createRouter({
   },
 })
 
-// console.log(routes)
+// 全局路由前置守卫
+router.beforeEach((to, from, next) => {
+  console.log(to)
+  console.log(from)
+  console.log(next)
+  // 进度条开始加载
+  nprogress.start()
+  next()
+})
 
+// 全局路由后置守卫
+router.afterEach((to, from) => {
+  // 进度条结束加载
+  nprogress.done()
+})
 // 分别导出
 export { routes }
 

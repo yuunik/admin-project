5a97b5be388729a96026be7c59c6f44226df55ba
yuunik <yuunik.zhou@gmail.com>
2024-05-06 14:41:27 +0800

    feat: 数据大屏 - 数据大屏实时游客统计

---
+++ src/views/screen/components/ChartBox/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+interface Props {
+  chartTitle: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="chart-box">
+    <div class="header">
+      <em class="chart-title">{{ chartTitle }}</em>
+      <i class="title-border"></i>
+    </div>
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.chart-box {
+  background: url(../../../../assets/images/screen/dataScreen-main-lb.png)
+    no-repeat 0 0 / 100% 100%;
+
+  .header {
+    .chart-title {
+      margin-left: 20px;
+      color: #fff;
+      font-size: 20px;
+    }
+
+    // 标题
+    .title-border {
+      width: 68px;
+      height: 7px;
+      margin: 10px 0 0 20px;
+      background: url(../../../../assets/images/screen/dataScreen-title.png)
+        no-repeat 0 0 / 100% 100%;
+      display: block;
+    }
+  }
+
+  .content {
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import ScreenHeader from './components/ScreenHeader/index.vue'
+import ChartBox from './components/ChartBox/index.vue'
 
 // 大屏模板实例
 const containerRef = ref<HTMLDivElement>()
@@ -44,6 +45,7 @@ const getFullScreen = () => {
 // 缩放方案四: 宽度铺满, 高度等比例放大
 
 // 缩放方案五: 高度铺满, 宽度等比例放大
+const visitorNumber = ref<string>('215908')
 </script>
 
 <template>
@@ -55,15 +57,36 @@ const getFullScreen = () => {
       </header>
       <!-- 数据大屏内容区 -->
       <main class="content">
+        <!-- 左侧图表 -->
         <div class="chart-left">
-          <div class="visitor-statistics">实时游客统计</div>
-          <div class="gender-ratio">男女比例</div>
-          <div class="age-ratio">年龄比例</div>
+          <ChartBox chart-title="实时游客统计" class="visitor-statistics">
+            <div class="sub-title">
+              可预约总人数
+              <em style="color: #965d44">99999</em>
+              人
+            </div>
+            <div class="reservations-made">
+              <span
+                v-for="(item, index) in visitorNumber"
+                :key="index"
+                class="number"
+              >
+                {{ item }}
+              </span>
+              <span class="unit">人</span>
+            </div>
+          </ChartBox>
+          <ChartBox chart-title="男女比例" class="gender-ratio" />
+          <ChartBox chart-title="年龄比例" class="age-ratio" />
         </div>
+        <!-- 中间内容区 -->
         <div class="center">
+          <!-- 地图 -->
           <div class="map">map</div>
+          <!-- 游客趋势图 -->
           <div class="chart-tendencies">tendencies</div>
         </div>
+        <!-- 右侧图表 -->
         <div class="chart-right">right</div>
       </main>
     </div>
@@ -71,11 +94,13 @@ const getFullScreen = () => {
 </template>
 
 <style scoped lang="scss">
+// 数据大屏
 .screen {
   width: 100vw;
   height: 100vh;
   background: url(../../assets/images/screen/bg.png) no-repeat 0 0 / 100% 100%;
 
+  // 大屏容器
   .container {
     width: 1920px;
     height: 1080px;
@@ -86,14 +111,90 @@ const getFullScreen = () => {
     display: flex;
     flex-wrap: wrap;
 
+    // 大屏顶部
     .page-header {
       width: 100%;
       height: 50px;
       display: flex;
     }
 
+    // 大屏内容区
     .content {
-      height: calc(100% - 50px);
+      display: flex;
+      width: 1920px;
+      justify-content: space-between;
+
+      // 左侧内容区
+      .chart-left {
+        width: 450px;
+        height: 1010px;
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+
+        // 实时游客统计
+        .visitor-statistics {
+          flex: 1;
+
+          &::after {
+            content: '';
+            display: block;
+            clear: both;
+          }
+
+          .sub-title {
+            float: right;
+            color: #fff;
+            margin-right: 10px;
+          }
+
+          .reservations-made {
+            width: 100%;
+            position: relative;
+            top: 10px;
+            font-size: 20px;
+            color: #29f6fa;
+            display: flex;
+            padding: 0 10px;
+            box-sizing: border-box;
+
+            .number {
+              flex: 1;
+              text-align: center;
+              height: 40px;
+              line-height: 40px;
+              background: url(../../assets/images/screen/total.png) no-repeat 0
+                0 / 100% 100%;
+            }
+
+            .unit {
+              flex: 1;
+              text-align: center;
+              height: 40px;
+              line-height: 40px;
+              background: url(../../assets/images/screen/total.png) no-repeat 0
+                0 / 100% 100%;
+            }
+          }
+        }
+
+        .gender-ratio {
+          flex: 1;
+          margin: 10px 0;
+        }
+
+        .age-ratio {
+          flex: 1;
+          margin-bottom: 10px;
+        }
+      }
+
+      .chart-right {
+        width: 450px;
+        height: 1010px;
+        margin-top: 20px;
+        background-color: yellowgreen;
+      }
     }
   }
 }
